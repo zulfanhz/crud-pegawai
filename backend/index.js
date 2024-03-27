397d27d7834759d46b8135c35a43cde8fa489d16
zulfanhz <161116040@mhs.stiki.ac.id>
2024-03-27 13:58:41 +0700

    balik awal

--- backend/index.js
+++ backend/index.js
@@ -1,13 +1,11 @@
 import express  from "express";
 import cors from "cors";
-import UserRoute from "../routes/UserRoute.js";
+import UserRoute from "./routes/UserRoute.js";
 
 const app = express();
-app.get("/", (req,res)=> res.send("Express on vercel"));
+
 app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
 
 app.listen(5000, ()=> console.log('Server up and running...'));
-
-module.exports = app;
