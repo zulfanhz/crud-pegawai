8036d7da24e1be743a69c9832291a6ee04d61977
zulfanhz <161116040@mhs.stiki.ac.id>
2024-03-27 13:29:53 +0700

    updaate

--- backend/api/index.js
+++ backend/api/index.js
@@ -3,6 +3,7 @@ import cors from "cors";
 import UserRoute from "../routes/UserRoute.js";
 
 const app = express();
+app.get("/", (req,res)=> res.send("Express on vercel");)
 app.use(cors());
 app.use(express.json());
 app.use(UserRoute);
