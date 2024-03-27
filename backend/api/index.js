d077fac3b02915750b32cf7aa59cc0b3e5db1af8
zulfanhz <161116040@mhs.stiki.ac.id>
2024-03-27 13:21:42 +0700

    updt

--- backend/api/index.js
+++ backend/api/index.js
@@ -1,6 +1,6 @@
 import express  from "express";
 import cors from "cors";
-import UserRoute from "./routes/UserRoute.js";
+import UserRoute from "../routes/UserRoute.js";
 
 const app = express();
 app.use(cors());
